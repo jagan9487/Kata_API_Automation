414ef5e56829212620725710910b46ca7548385b
Jagan9487 <jackychandran09@gmail.com>
2026-03-26 17:13:29 +0530

    Add BookingDates DTO for checkin and checkout fields

---
+++ src/test/java/com/booking/models/BookingDates.java
@@ -0,0 +1,17 @@
+package com.booking.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class BookingDates {
+        private String checkin;
+        private String checkout;
+    }
+
+
+
