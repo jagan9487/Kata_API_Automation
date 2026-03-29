330fcb07fba3cd837644c5429aa220a65cad52fd
Jagan9487 <jackychandran09@gmail.com>
2026-03-29 20:53:29 +0530

    Update steps path and tags in runner file

--- src/test/java/com/booking/runners/TestRunner.java
+++ src/test/java/com/booking/runners/TestRunner.java
@@ -1,10 +1,14 @@
 package com.booking.runners;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+    @RunWith(Cucumber.class)
     @CucumberOptions(
             features = "src/test/resources/features",
-            glue = "steps",
+            glue = "com.booking.steps",
+            tags = "@UpdateBooking",
             plugin = {"pretty", "html:target/cucumber-report.html"}
     )
     public class TestRunner{
